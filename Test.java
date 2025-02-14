261431c302ace8dbcb1cf8d3cf396e695570544d
samiksha603 <samikshamohapatra7@gmail.com>
2025-02-14 05:59:15 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test {
+ public static void main(String[] agrs) {
+
+    int a=10, b=20;
+
+    System.out.println("Before swapping values.."+a+" "+b);
+
+    int swap=a;
+    a=b;
+    b=swap;
+
+    System.out.println("After swapping.."+a+" "+b);
+ }
+    
+}
